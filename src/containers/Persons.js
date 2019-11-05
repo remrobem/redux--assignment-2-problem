f92cbf1d929f5943bea68fbb709050018dceba11
Rob Martin <rob.martin.5758@gmail.com>
2019-11-05 12:40:48 -0500

    completed exersize

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,44 +1,73 @@
 import React, { Component } from 'react';
+import * as actionTypes from '../store/actions';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
 
+// new import
+import { connect } from 'react-redux';
+
 class Persons extends Component {
-    state = {
-        persons: []
-    }
+    // state not needed here. in the reducer
+    // state = {
+    //     persons: []
+    // }
 
-    personAddedHandler = () => {
-        const newPerson = {
-            id: Math.random(), // not really unique but good enough here!
-            name: 'Max',
-            age: Math.floor( Math.random() * 40 )
-        }
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.concat(newPerson)}
-        } );
-    }
+    // logic moved to the reducer
+    // personAddedHandler = () => {
+    //     const newPerson = {
+    //         id: Math.random(), // not really unique but good enough here!
+    //         name: 'Max',
+    //         age: Math.floor(Math.random() * 40)
+    //     }
+    //     this.setState((prevState) => {
+    //         return { persons: prevState.persons.concat(newPerson) }
+    //     });
+    // }
 
-    personDeletedHandler = (personId) => {
-        this.setState( ( prevState ) => {
-            return { persons: prevState.persons.filter(person => person.id !== personId)}
-        } );
-    }
+    // personDeletedHandler = (personId) => {
+    //     this.setState((prevState) => {
+    //         return { persons: prevState.persons.filter(person => person.id !== personId) }
+    //     });
+    // }
 
-    render () {
+    render() {
         return (
             <div>
-                <AddPerson personAdded={this.personAddedHandler} />
-                {this.state.persons.map(person => (
-                    <Person 
+                {/* <AddPerson personAdded={this.personAddedHandler} /> */}
+                <AddPerson personAdded={this.props.onAddPerson} />
+                {/* {this.state.persons.map(person => ( */}
+                {this.props.personsArray.map(person => (
+                    <Person
                         key={person.id}
-                        name={person.name} 
-                        age={person.age} 
-                        clicked={() => this.personDeletedHandler(person.id)}/>
+                        name={person.name}
+                        age={person.age}
+                        clicked={() => this.props.onRemovePerson(person.id)} />
                 ))}
             </div>
         );
     }
 }
 
-export default Persons;
+// connect is function that returns a high-order componentbundleRenderer.
+// pass the slice of state (or store) this component uses as props and the actions used
+
+const mapStateToProps = state => {
+    return {
+        personsArray: state.persons
+    }
+}
+// dispatch actions
+// anonymous function to execute when action taken
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPerson: () => {
+            return dispatch({ type: actionTypes.ADD_PERSON })
+        },
+        onRemovePerson: (id) => {
+            return dispatch({ type: actionTypes.REMOVE_PERSON, personId: id })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// new import
+import {createStore} from 'redux';
+import reducer from './store/reducer';
+import { Provider } from 'react-redux';
+
+// initialize redux store using the reducer (changes state)
+const store = createStore(reducer);
+
+// App wrapped in Provider from react-redux with the store specified
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
